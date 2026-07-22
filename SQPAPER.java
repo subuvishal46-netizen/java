d1bfb74ba8de4fd99e8606cddc3ec5cfbbcd0593
subuvishal46-netizen <subuvishal46@gmail.com>
2026-07-22 15:33:34 +0530

    Add files via upload

---
+++ SQPAPER.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt(),b=sc.nextInt();
+	    if (a<b){
+	        System.out.println(a*a);
+	    }
+	    else{
+	        System.out.println(b*b);
+	    }
+	}
+}
