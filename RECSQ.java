ec6602d205bd411ee13c02a64d65abaa50a42b00
subuvishal46-netizen <subuvishal46@gmail.com>
2026-07-22 15:48:57 +0530

    Add files via upload

---
+++ RECSQ.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+	    if(a*b==c*c){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+
+	    }
+	}
+}
