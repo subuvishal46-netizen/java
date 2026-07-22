d5418a7cb240d95b9fd54ae4952115fd14ac390f
subuvishal46-netizen <subuvishal46@gmail.com>
2026-07-22 16:25:35 +0530

    Add files via upload

---
+++ GAMBLING.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    System.out.print(7-a);
+
+	}
+}
