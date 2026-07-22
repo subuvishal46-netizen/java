01bb5ef361629964c4f312d908c4956c31ad121a
subuvishal46-netizen <subuvishal46@gmail.com>
2026-07-22 16:16:55 +0530

    Add files via upload

---
+++ CLSI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    if(a>=b*10){
+	        System.out.println("YES");
+	    }else{
+	        System.out.println("NO");
+	    }
+	}
+}
