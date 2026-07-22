9cd99bf492166f738e620aabb405d3ab1b9cf09c
subuvishal46-netizen <subuvishal46@gmail.com>
2026-07-22 16:00:15 +0530

    Add files via upload

---
+++ VACAEX.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt(),b=sc.nextInt();
+	    System.out.println((a+b-1));
+    
+	}
+}
